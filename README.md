cda345f924bb3f9e1c6512b7b3955b5cc74d408b
Augustin Barbe <augustin.barbe@gmail.com>
2017-10-15 23:50:31 +0200

    updating readme

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 chess-web is the web client for the entire application. It takes cares of displaying the board and updating positions while the battle is occuring.
 
 It relies on flask, including a celery job queuer to start the battle. A communication with a redis instance is required as updating positions relies on a publish–subscribe pattern.
+
+## Installation
+
+```
+git clone git@github.com:augustinbarbe/chess-web.git
+cd dockerweb-master
+docker build -t dockerweb:dev .
+docker run -p 5000:8000 dockerweb:dev
+```
--- chessweb/static/js/main-board.js
+++ chessweb/static/js/main-board.js
@@ -1,18 +1,18 @@
 $(document).ready( function() {
-    var board1 = ChessBoard('board1', 'start');
-    // var board1 = ChessBoard('board1', {
+    var board1 = ChessBoard('board1', {
     
-    //     draggable: true,
-    //     dropOffBoard: 'trash',
-    //     sparePieces: true
-    // });
+        draggable: true,
+        dropOffBoard: 'trash',
+        sparePieces: true
+    });
 
     $('#startBtn').on('click', function() {
         board1.start();
+        board1.sparePieces = false;
         $.post('/create', { 'message' : "empty"} );
     });
     
     $('#clearBtn').on('click', function() {
-
+        board1.clear()
     });
 });
